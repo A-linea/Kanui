15e0ec532d569fa4aa6790b5a4b2944ba90594e4
Yevhen Dmitrievskiy <ravenio@ex.ua>
2018-05-30 17:52:33 +0300

    fix nav bar, map, articles

--- source/js/app.js
+++ source/js/app.js
@@ -1,19 +1,44 @@
+import {accord} from './components/accord';
 import {mPageScroll2id} from 'page-scroll-to-id';
-import accord from './components/accord';
-$(document).ready(() => {
-  // @TODO delete this
-  let str = `window location is ${window.location}`;
-  console.log(str);
 
+$(document).ready(() => {
 
   $(".main-nav__link").mPageScroll2id({
     offset: 0,
-    highlightClass:"main-nav__link--active"
+    highlightClass: "main-nav__link--active"
   });
 
-
+  $('.form__input--resource').focus(
+    function () {
+      $(this).parent('div').addClass('form__input-wrapper--focus');
+    }).blur(
+    function () {
+      $(this).parent('div').removeClass('form__input-wrapper--focus');
+    });
 });
 
-// accord('.dropdown');
+const navBar = $(".header");
+const link = $(".main-nav__link");
+const logoMain = $(".logo");
+const logoNav = $(".logo--nav-bar");
+const linkedinIcon =$(".icon--ln");
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 360) {
+    navBar.addClass("header--is-active");
+    link.addClass("main-nav__link--bar");
+    logoMain.addClass("logo--hidden");
+    logoNav.addClass("logo--nav-bar-is-active");
+    linkedinIcon.addClass("icon--black");
+  } else {
+    navBar.removeClass("header--is-active");
+    link.removeClass("main-nav__link--bar");
+    logoMain.removeClass("logo--hidden");
+    logoNav.removeClass("logo--nav-bar-is-active");
+    linkedinIcon.removeClass("icon--black");
+  }
+});
+
+accord('dropdown');
 
 
--- source/js/components/accord.js
+++ source/js/components/accord.js
@@ -1,11 +1,12 @@
 // features section accordion
-const accord = (element) => {
+export const accord = element => {
   const acc = document.getElementsByClassName(element);
   let i;
   for (i = 0; i < acc.length; i++) {
     acc[i].onclick = function() {
       this.classList.toggle(`${element}-is--active`);
       const panel = this.nextElementSibling;
+      console.log(panel);
       if (panel.style.maxHeight){
         panel.style.maxHeight = null;
       } else {
